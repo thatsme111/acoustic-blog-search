af41c006cbb6bdad727464a496825d5bbc8c1145
shailesh <nighojkar111@gmail.com>
2021-04-10 17:05:08 +0530

    created blogs crawler

---
+++ blogs-crawler/crawler.js
@@ -0,0 +1,50 @@
+const axios = require("axios");
+const puppeteer = require('puppeteer');
+const xml2js = require("xml2js");
+const fs = require("fs");
+
+const getAllBlogsUrls = async () => {
+    const sitemap = "https://acoustic.com/sitemap.xml";
+    const response = await axios.get(sitemap);
+    const parsedSiteMap = await xml2js.parseStringPromise(response.data);
+    return parsedSiteMap.urlset.url
+        .map(e => e.loc[0])
+        .filter(e => e.includes("/blog/"));
+};
+
+const parseBlog = async (page, url) => {
+    await page.goto(url);
+    await page.waitForSelector('.gradient-hero__title', { timeout: 1000 });
+    return page.evaluate(() => {
+        const check = e => e ? e : { children: [], getAttribute: () => { } };
+        return {
+            url: document.location.href,
+            title: check(document.querySelector('.gradient-hero__title')).innerHTML,
+            date: check(document.querySelector('.resource-meta-info__date')).innerHTML,
+            author: {
+                name: check(document.querySelector('.resource-meta-info__label')).innerHTML,
+                job: check(document.querySelector('.contributor-info__job')).innerHTML,
+                imageUrl: check(document.querySelector('.contributor-info__headshot > picture > img')).getAttribute("data-src"),
+            },
+            content: Array.from(check(document.querySelector(".basic-text__inner")).children).map(e => e.textContent)
+        }
+    });
+}
+
+(async () => {
+    const padIndex = index => (index < 10 ? '0' : '') + index;
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        const urls = await getAllBlogsUrls();
+        for ([index, url] of urls.entries()) {
+            console.log(padIndex(index), url);
+            const blog = await parseBlog(page, url);
+            const fileName = `blogs/${padIndex(index)} ${url.substr(url.indexOf("blog/") + "blog/".length)}`;
+            fs.writeFileSync(fileName, JSON.stringify(blog));
+        }
+        await browser.close();
+    } catch (error) {
+        console.log(error);
+    }
+})();
